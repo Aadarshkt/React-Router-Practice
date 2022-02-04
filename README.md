3f252428840f3a7bb791a2878aa193d2993b2531
Aadarshkt <aadarshkt1729@gmail.com>
2022-02-04 14:24:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-aoz9tq-ubfzkp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-aoz9tq-ubfzkp)
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './main.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
